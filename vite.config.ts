5d31b79ed08d359277d31acf5374109e2a13dad5
Kayla Blosser <kayla.blosser@proton.me>
2025-02-06 14:27:50 -0500

    Add postcss configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import discardUnused from 'postcss-discard-unused';
+import cssnano from 'cssnano';
+import colormin from 'postcss-colormin';
+import discardDuplicates from 'postcss-discard-duplicates';
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
     ],
-    base: '/Turquoise/'
+    base: '/Turquoise/',
+    css: {
+        postcss: {
+            plugins: [
+                cssnano({
+                    plugins: [
+                        discardUnused,
+                        colormin,
+                        discardDuplicates
+                    ]
+                })
+            ]
+        }
+    }
 })
